9172decb4003467a4fad696db5759c048ae976f7
lifeofatom <perfect.igbadumhe@gmail.com>
2020-04-11 16:31:49 +0100

    first

--- src/estimator.py
+++ src/estimator.py
@@ -1,16 +1,18 @@
+import math
+
 region = {
     "name":"Africa",
     "avgAge":19.7,
-    "avgDailyIncomeInUSD": 5,
-    "avgDailyIncomePopulation":0.71
+    "avgDailyIncomeInUSD": 4,
+    "avgDailyIncomePopulation":0.73
 }
 
 
 periodType = "days"
-timeToElapse = 58
-reportedCases = 674
-population = 66622705
-totalHospitalBeds = 1380614
+timeToElapse = 38
+reportedCases = 2747
+population = 92931687
+totalHospitalBeds = 678874
 
 data = {
     "region":region,
@@ -22,18 +24,77 @@ data = {
 }
 
 
+def timeFrame(data):
+  period = data["periodType"]
+  value = data["timeToElapse"]
+  if period == "months":
+    value = value * 30
+  elif period == "weeks":
+    value = value * 7
+  else:
+    value = data["timeToElapse"]
+  return value
+
+
 
 def infectionTime(data, currentlyInfected):
-    integralPeriod = int((data['timeToElapse'])/3)
-    period = currentlyInfected * (2**integralPeriod)
+    valueTime = (timeFrame(data))/3
+    integralPeriod = math.trunc(valueTime)
+    period = math.trunc(currentlyInfected * (2**integralPeriod))
     return period
+
+
+
+
+def severeCases(infectionsByRequestedTime):
+  cases = round(0.15 * infectionsByRequestedTime)
+  return cases
+
+def requestedBeds(data, severeCasesByRequestedTime):
+  totalSevereBeds = round(0.35 * data["totalHospitalBeds"])
+  cases = math.trunc(totalSevereBeds - severeCasesByRequestedTime)
+  return cases
+
+def icuRequestTime(infectionsByRequestedTime):
+  cases = round(0.05 * infectionsByRequestedTime)
+  cases = math.trunc(cases)
+  return cases
+
+def ventilatorRequestTime(infectionsByRequestedTime):
+  cases = round(0.02 * infectionsByRequestedTime)
+  cases =math.trunc(cases)
+  return cases
+
+def moneySpent(data, infectionsByRequestedTime):
+  dataRegions = data['region']
+  avgIncome = dataRegions['avgDailyIncomeInUSD']
+  avgIncomePopulation = dataRegions['avgDailyIncomePopulation']
+  period = timeFrame(data)
+  value = avgIncome * avgIncomePopulation * period * infectionsByRequestedTime
+  amount = round(value, 2)
+  return amount
+
+
+
+
 
 def normalImpact(data):
     currentlyInfected = data['reportedCases'] * 10
     infectionsByRequestedTime = infectionTime(data, currentlyInfected)
+    severeCasesByRequestedTime = severeCases(infectionsByRequestedTime)
+    hospitalBedsByRequestedTime = requestedBeds(data, severeCasesByRequestedTime)
+    casesForICUByRequestedTime = icuRequestTime(infectionsByRequestedTime)
+    casesForVentilatorsByRequestedTime = ventilatorRequestTime(infectionsByRequestedTime)
+    dollarsInFlight = moneySpent(data, infectionsByRequestedTime)
+
     data = {
         "currentlyInfected":currentlyInfected,
         "infectionsByRequestedTime":infectionsByRequestedTime,
+        "severeCasesByRequestedTime":severeCasesByRequestedTime,
+        "hospitalBedsByRequestedTime": hospitalBedsByRequestedTime,
+        "casesForICUByRequestedTime": casesForICUByRequestedTime,
+        "casesForVentilatorsByRequestedTime": casesForVentilatorsByRequestedTime,
+        "dollarsInFlight":dollarsInFlight
     }
     
     return data
@@ -41,9 +102,21 @@ def normalImpact(data):
 def worseImpact(data):
     currentlyInfected = data['reportedCases'] * 50
     infectionsByRequestedTime = infectionTime(data, currentlyInfected)
+    severeCasesByRequestedTime = severeCases(infectionsByRequestedTime)
+    hospitalBedsByRequestedTime = requestedBeds(data, severeCasesByRequestedTime)
+    casesForICUByRequestedTime = icuRequestTime(infectionsByRequestedTime)
+    casesForVentilatorsByRequestedTime = ventilatorRequestTime(infectionsByRequestedTime)
+    dollarsInFlight = moneySpent(data, infectionsByRequestedTime)
+
+
     data = {
         "currentlyInfected":currentlyInfected,
-        "infectionsByRequestedTime": infectionsByRequestedTime
+        "infectionsByRequestedTime": infectionsByRequestedTime,
+        "severeCasesByRequestedTime":severeCasesByRequestedTime,
+        "hospitalBedsByRequestedTime": hospitalBedsByRequestedTime,
+        "casesForICUByRequestedTime": casesForICUByRequestedTime,
+        "casesForVentilatorsByRequestedTime": casesForVentilatorsByRequestedTime,
+        "dollarsInFlight": dollarsInFlight
     }
     
     return data
